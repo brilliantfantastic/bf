6b1c7e7c872ab57a8af89a0ccd96c67564a4d597
Jamie Wright <jamie@brilliantfantastic.com>
2019-02-19 23:17:35 -0500

    Global style to be used

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
+import GlobalStyles from "../../layout/GlobalStyles"
 import Header from "../Header"
 
 const Layout = ({ children }) => (
@@ -17,6 +18,7 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <div>
+        <GlobalStyles />
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
           style={{
