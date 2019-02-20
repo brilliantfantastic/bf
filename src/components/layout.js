6050dcaa0e13aa1e396cc3aa3327940c20ef7bb7
Jamie Wright <jamie@brilliantfantastic.com>
2019-02-19 22:20:06 -0500

    Removed several footer items

--- src/components/layout.js
+++ src/components/layout.js
@@ -28,9 +28,7 @@ const Layout = ({ children }) => (
         >
           {children}
           <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
+            © {new Date().getFullYear()} Brilliant Fantastic
           </footer>
         </div>
       </>
