65530c17aeffc9c4f93be3577a41cac3eccfe2bf
Jamie Wright <jamie@brilliantfantastic.com>
2019-02-22 18:35:18 -0500

    Adjust the font based on the media query

--- src/components/H1/index.js
+++ src/components/H1/index.js
@@ -2,9 +2,17 @@ import styled from "styled-components"
 
 const H1 = styled.h1`
   font-family: ${props => props.theme.fonts.heading};
-  font-size: 4.5rem;
+  font-size: 2.5rem;
   font-weight: 700;
   margin: 0;
+
+  ${props => props.theme.mq.tablet} {
+    font-size: 3.5rem;
+  }
+
+  ${props => props.theme.mq.desktop} {
+    font-size: 4.5rem;
+  }
 `;
 
 export default H1;
