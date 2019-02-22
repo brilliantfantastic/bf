74139b6d53ff93280bc9453e9c0e915e7a33001b
Jamie Wright <jamie@brilliantfantastic.com>
2019-02-22 18:13:11 -0500

    Display the site description as an H1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,32 @@
 import React from "react"
+import { StaticQuery, graphql } from "gatsby"
 
-import HeroImage from "../components/HeroImage";
+import HeroImage from "../components/HeroImage"
+import H1 from "../components/H1"
 import Layout from "../components/Layout"
 import SEO from "../components/SEO"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <HeroImage />
-  </Layout>
+  <StaticQuery
+    query={graphql`
+      query SiteDescriptionQuery {
+        site {
+          siteMetadata {
+            description
+          }
+        }
+      }
+    `}
+    render={data => (
+      <Layout>
+        <SEO title="Home" keywords={[`software company`]} />
+        <H1>
+          {data.site.siteMetadata.description}
+        </H1>
+        <HeroImage />
+      </Layout>
+    )}
+  />
 )
 
 export default IndexPage
