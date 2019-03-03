2689c73cf004aafc0ef139acac1054bf40696ed9
Jamie Wright <jamie@brilliantfantastic.com>
2019-03-03 12:54:16 -0500

    Burger menu now displays on click

--- src/components/BurgerTime/index.js
+++ src/components/BurgerTime/index.js
@@ -5,18 +5,16 @@ const Bun = styled.div`
   cursor: pointer;
 `
 
-const Patty = styled.div`
-  background-color: black;
-  height: 5px;
-  margin: 6px 0;
-  width: 35px;
-`
-
-const Burger = ({ onClick }) => (
+const Burger = ({ closed, onClick }) => (
   <Bun onClick={onClick}>
-    <Patty />
-    <Patty />
-    <Patty />
+    {closed &&
+      <React.Fragment>
+        <Patty />
+        <Patty />
+        <Patty />
+      </React.Fragment>
+    }
+    {!closed && <RotatedPatty />}
   </Bun>
 )
 
@@ -27,10 +25,58 @@ const MenuWrapper = styled.div`
 `
 
 const Menu = ({ children }) => (
-  <MenuWrapper>
-    {children}
-  </MenuWrapper>
+  <Overlay>
+    <MenuWrapper>
+      {children}
+    </MenuWrapper>
+  </Overlay>
 )
+
+const Overlay = styled.div`
+  background-color: rgba(0,0,0, 0.9);
+  height: 100%;
+  left: 0;
+  overflow-x: hidden;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 1;
+`
+
+const Patty = styled.div`
+  background-color: black;
+  height: 5px;
+  margin: 6px 0;
+  width: 35px;
+`
+
+const RotatedPatty = styled.div`
+  display: inline-block;
+  height: 35px;
+  overflow: hidden;
+  position: relative;
+  width: 35px;
+  z-index: 10;
+
+  &::before, &::after {
+    background-color: #fff;
+    content: "";
+    height: 5px;
+    left: 0;
+    margin-top: -1px;
+    position: absolute;
+    top: 50%;
+    width: 100%;
+  }
+
+  &::before {
+    transform: rotate(45deg);
+  }
+
+  &::after {
+    transform: rotate(-45deg);
+  }
+`
 
 class BurgerTime extends Component {
   state = {
@@ -48,7 +94,10 @@ class BurgerTime extends Component {
     const { children } = this.props;
 
     return (
-      closed ? <Burger onClick={this.toggleMenu} /> : <Menu children={children} />
+      <React.Fragment>
+        <Burger closed={closed} onClick={this.toggleMenu} />
+        {!closed && <Menu children={children} />}
+      </React.Fragment>
     )
   }
 }
