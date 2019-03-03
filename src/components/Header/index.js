3b2846febd0a679b3856d4852a4794958503c123
Jamie Wright <jamie@brilliantfantastic.com>
2019-03-03 13:22:19 -0500

    Initial menu items

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -19,6 +19,44 @@ const LogoLink = styled(Link)`
   text-decoration: none;
 `
 
+const MenuContents = styled.ul`
+  color: white;
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+  list-style-type: none;
+  margin: 0 auto;
+  padding: 0;
+  text-align: center;
+`
+
+const MenuHeading = styled.h3`
+  font-family: ${props => props.theme.fonts.heading};
+  font-size: 3rem;
+  font-weight: 400;
+  letter-spacing: 0.2em;
+  line-height: 1.2;
+  text-transform: uppercase;
+`
+
+const MenuHeader = ({ children }) => (
+  <li>
+    <MenuHeading>{children}</MenuHeading>
+  </li>
+)
+
+const MenuItem = styled.li`
+  font-size: 2rem;
+  padding: 16px 0;
+`
+
+const MenuSection = styled.ul`
+  list-style-type: none;
+  margin: 0 auto;
+  padding: 0;
+  text-align: center;
+`
+
 const MenuWrapper = styled.div`
   align-self: flex-end;
 `
@@ -30,7 +68,17 @@ const Header = ({ siteTitle }) => (
     </LogoLink>
     <MenuWrapper>
       <Menu>
-        <div>Menu goes here, yo</div>
+        <MenuContents>
+          <MenuHeader>Work</MenuHeader>
+          <MenuSection>
+            <MenuItem>Latest</MenuItem>
+            <MenuItem>Chronic</MenuItem>
+            <MenuItem>Tatsu</MenuItem>
+          </MenuSection>
+          <MenuHeader>About</MenuHeader>
+          <MenuHeader>Blog</MenuHeader>
+          <MenuHeader>Reach Out</MenuHeader>
+        </MenuContents>
       </Menu>
     </MenuWrapper>
   </HeaderWrapper>
