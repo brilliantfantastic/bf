bb13699cacc6e4bd456ecb98034056b2ebf7e022
Jamie Wright <jamie@brilliantfantastic.com>
2019-03-03 16:29:39 -0500

    Extract Menu to it's own component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,9 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
 import styled from "styled-components"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
 
-import Menu from "../BurgerTime"
+import Menu from "../Menu"
 
 const HeaderWrapper = styled.div`
   display: flex;
@@ -19,68 +19,12 @@ const LogoLink = styled(Link)`
   text-decoration: none;
 `
 
-const MenuContents = styled.ul`
-  color: ${props => props.theme.colors.background};
-  display: flex;
-  height: 100vh;
-  flex-direction: column;
-  list-style-type: none;
-  margin: 0 auto;
-  padding: 0;
-  text-align: center;
-`
-
-const MenuHeading = styled.h3`
-  font-family: ${props => props.theme.fonts.heading};
-  font-size: 3rem;
-  font-weight: 400;
-  letter-spacing: 0.2em;
-  line-height: 1.2;
-  text-transform: uppercase;
-`
-
-const MenuHeader = ({ children }) => (
-  <li>
-    <MenuHeading>{children}</MenuHeading>
-  </li>
-)
-
-const MenuItem = styled.li`
-  font-size: 2rem;
-  padding: 16px 0;
-`
-
-const MenuSection = styled.ul`
-  list-style-type: none;
-  margin: 0 auto;
-  padding: 0;
-  text-align: center;
-`
-
-const MenuWrapper = styled.div`
-  align-self: flex-end;
-`
-
-const Header = ({ siteTitle }) => (
+const Header = ({ foreColor, siteTitle }) => (
   <HeaderWrapper>
     <LogoLink to="/">
       {siteTitle}
     </LogoLink>
-    <MenuWrapper>
-      <Menu>
-        <MenuContents>
-          <MenuHeader>Work</MenuHeader>
-          <MenuSection>
-            <MenuItem>Latest</MenuItem>
-            <MenuItem>Chronic</MenuItem>
-            <MenuItem>Tatsu</MenuItem>
-          </MenuSection>
-          <MenuHeader>About</MenuHeader>
-          <MenuHeader>Blog</MenuHeader>
-          <MenuHeader>Reach Out</MenuHeader>
-        </MenuContents>
-      </Menu>
-    </MenuWrapper>
+    <Menu foreColor={foreColor} />
   </HeaderWrapper>
 )
 
