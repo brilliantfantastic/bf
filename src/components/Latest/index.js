dd79e2c5a7442e2f6978778c10490f2f37197b96
Jamie Wright <jamie@brilliantfantastic.com>
2019-03-03 21:30:00 -0500

    The latest initial section with image

--- src/components/Latest/index.js
+++ src/components/Latest/index.js
@@ -1,22 +1,48 @@
 import React from "react"
 import styled from 'styled-components'
+import Img from "gatsby-image"
+import { StaticQuery, graphql } from "gatsby"
 
 const H3 = styled.h3`
   font-family: ${props => props.theme.fonts.heading};
   font-size: 3rem;
   font-weight: 400;
+  margin: 0 auto;
   text-align: center;
   width: 100%;
 `
 
 const LatestWrapper = styled.div`
   display: flex;
+  padding: 2em 1em;
 `
 
 const Latest = () => (
-  <LatestWrapper>
-    <H3>The Latest</H3>
-  </LatestWrapper>
+  <StaticQuery
+    query={graphql`
+      query LatestHeaderImageQuery {
+        file(relativePath: { eq: "latest.png" }) {
+          childImageSharp {
+            fixed(width: 100) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <LatestWrapper>
+        <H3>
+          <Img
+            fixed={data.file.childImageSharp.fixed}
+            alt="Latest Work"
+            style={{ display: `inherit`, margin: `0 auto` }}
+          />
+          The Latest
+        </H3>
+      </LatestWrapper>
+    )}
+  />
 )
 
 export default Latest
