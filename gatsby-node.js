8bfe00a65e8024c76ea6e1c224568aa4026fcc30
Jamie Wright <jamie@brilliantfantastic.com>
2019-03-07 08:46:30 -0500

    Create the blog post pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,40 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require("path")
 
-// You can delete this file if you're not using it
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  return new Promise((resolve, reject) => {
+    const blogPostTemplate = path.resolve("src/templates/BlogPost/index.js")
+    resolve(
+      graphql(`
+        {
+          allMarkdownRemark(
+            sort: { order: DESC, fields: [frontmatter___date] }
+          ) {
+            edges {
+              node {
+                frontmatter {
+                  slug
+                }
+              }
+            }
+          }
+        }
+      `).then(result => {
+        if (result.errors) {
+          reject(result.errors)
+        }
+        result.data.allMarkdownRemark.edges.forEach(edge => {
+          createPage({
+            path: `blog/${edge.node.frontmatter.slug}`,
+            component: blogPostTemplate,
+            context: {
+              slug: edge.node.frontmatter.slug,
+            },
+          })
+        })
+        return
+      })
+    )
+  })
+}
