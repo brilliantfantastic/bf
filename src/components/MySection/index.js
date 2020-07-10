94224757436f3e02f1827f066e2cc68343ffeb49
Jamie Wright <jamie@brilliantfantastic.com>
2020-07-10 16:25:25 -0400

    WIP: Start implementing new hero section

---
+++ src/components/MySection/index.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+import ContentWrapper from "../ContentWrapper"
+import H1 from "../H1"
+
+const MySection = () => (
+  <ContentWrapper>
+    <H1>My name is Jamie...</H1>
+    <div>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porta fermentum diam in ultrices. Morbi sit amet urna nulla. Maecenas quis odio tristique urna feugiat vulputate. Pellentesque condimentum nunc vel ornare egestas. Cras at ante porta, efficitur arcu sed, condimentum nisi. Etiam lobortis lacus hendrerit, convallis libero eu, imperdiet ipsum. Ut a aliquam orci, eu mollis ligula. Aliquam erat volutpat. Ut et dui ut dolor placerat varius.
+      </p>
+      <p>
+Donec ac tellus mollis, egestas nisi mollis, vulputate odio. Nullam lobortis fringilla porttitor. Pellentesque ut lacus et risus aliquam scelerisque. Pellentesque id tristique sem, at condimentum elit. Curabitur quis sagittis sapien. Curabitur eget faucibus odio. Nullam mattis arcu sed arcu feugiat egestas. Duis nec ullamcorper arcu.
+      </p>
+    </div>
+  </ContentWrapper>
+)
+
+export default MySection
