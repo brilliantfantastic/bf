9fa979ae86442c1db40b3cdc418a7de53f961000
Jamie Wright <jamie@brilliantfantastic.com>
2020-07-21 09:16:49 -0400

    WIP: Temporary image to get the effect correct

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,11 +1,12 @@
 import React from "react"
 
+import heroImage from "../../images/clay.png"
 import theme from "../../layout/theme"
 
 import H1 from "../H1"
 
 const HeroSection = () => (
-  <main className="relative h-screen" style={{ backgroundColor: `${theme.colors.bfPink}` }}>
+  <div className="relative h-screen" style={{ backgroundColor: `${theme.colors.bfPink}` }}>
     <div className="mt-8 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-20 xl:mt-24 ">
       <div className="lg:grid lg:grid-cols-12 lg:gap-8">
         <div className="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
@@ -14,7 +15,10 @@ const HeroSection = () => (
         </div>
       </div>
     </div>
-  </main>
+    <div className="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full">
+      <img className="absolute inset-0 w-full h-full object-cover" src={heroImage} alt="claymation brill fan" />
+    </div>
+  </div>
 )
 
 export default HeroSection
