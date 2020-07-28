efe8df2729c85e609c2732628892d023d9ea66ef
Jamie Wright <jamie@brilliantfantastic.com>
2020-07-28 07:20:48 -0400

    Extend the color and font sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,19 @@ module.exports = {
   purge: [],
   theme: {
     extend: {
+      colors: {
+        "bf-green": "#a1d043",
+        "bf-pink": "#ef73a5",
+      },
       fontFamily: {
         body: ["LacrimaSenza", "monospace"],
         heading: ["OmnesPro", "sans-serif"],
+      },
+      fontSize: {
+        "7xl": "5rem",
+        "8xl": "6rem",
+        "9xl": "7rem",
+        "10xl": "8rem",
       },
     },
   },
