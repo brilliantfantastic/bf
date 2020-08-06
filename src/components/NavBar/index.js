f6bc065c2eb7f3949f457c676b28dbd3a053ff9d
Jamie Wright <jamie@brilliantfantastic.com>
2020-08-05 21:22:08 -0400

    Modify some of the menu

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -45,12 +45,24 @@ const NavBar = ({ backgroundColor = "white",
               </div>
             </div>
             <div class="-mr-2 flex items-center sm:hidden">
-              <button class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out" aria-label="Main menu" aria-expanded="false">
-                <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+              <button class={`inline-flex
+                              items-center
+                              justify-center
+                              p-4
+                              rounded-md
+                              text-black
+                              focus:outline-none
+                              transition
+                              duration-150
+                              ease-in-out`}
+                      aria-label="Main menu"
+                      aria-expanded="false"
+              >
+                <svg class="block h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
-                <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                <svg class="hidden h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M6 18L18 6M6 6l12 12" />
                 </svg>
               </button>
             </div>
