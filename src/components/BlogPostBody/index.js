78c08960ac2fc72620ace39f3608eca6cffd75f7
Jamie Wright <jamie@brilliantfantastic.com>
2020-08-07 16:11:32 -0400

    Add a stronger css to the strong node

--- src/components/BlogPostBody/index.js
+++ src/components/BlogPostBody/index.js
@@ -3,6 +3,11 @@ import styled from "styled-components"
 const BlogPostBody = styled.div`
   line-height: 1.4;
   padding: 16px 0;
+
+  * > strong {
+    font-size: larger;
+    font-weight: bold;
+  }
 `
 
 export default BlogPostBody
