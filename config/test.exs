d7de2856cbdad51043031d46ea890a7617e7b9e9
Jamie Wright <jamie@brilliantfantastic.com>
2026-07-29 17:00:24 -0400

    Use the database port specified or the default

--- config/test.exs
+++ config/test.exs
@@ -9,6 +9,7 @@ config :bf, BrilliantFantastic.Repo,
   username: "postgres",
   password: "postgres",
   hostname: "localhost",
+  port: String.to_integer(System.get_env("DATABASE_PORT") || "5432"),
   database: "bf_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
